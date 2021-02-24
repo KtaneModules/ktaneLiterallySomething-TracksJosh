a482ebb882d70514ba9dd63b61811620cccb55d5
TracksJosh <arcticwolf1526@gmail.com>
2021-02-24 14:00:46 -0600

    Almost ready
    Almost ready for initial release

--- Assets/literallySomethingScript.cs
+++ Assets/literallySomethingScript.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using System;
 using KModkit;
 using System.Text.RegularExpressions;
+using System.Text;
+using System.Linq;
 
 public class literallySomethingScript : MonoBehaviour {
 
@@ -11,7 +13,7 @@ public class literallySomethingScript : MonoBehaviour {
     public KMBombModule bombModule;
     public Material[] colorful;
     public Renderer[] ColorRender;
-    public KMBombInfo BombInfo;
+    public KMBombInfo Bomb;
 
     public KMSelectable S;
     public KMSelectable O;
@@ -38,31 +40,78 @@ public class literallySomethingScript : MonoBehaviour {
     private bool status;
     private bool isActive;
 
+    public double[] numb = new double[9];
+
+    private int serInt1;
+    private int serInt2;
+    private int serInt3;
+    private int serInt4;
+    private int serInt5;
+    private int serInt6;
+    private int serInt7;
+    private int serInt8;
+    private int serInt9;
+    private int sumOfDigits;
 
     private int letterValues;
-    private int SletterValue;
-    private int OletterValue;
-    private int MletterValue;
-    private int EletterValue;
-    private int TletterValue;
-    private int HletterValue;
-    private int IletterValue;
-    private int NletterValue;
-    private int GletterValue;
+    private double SletterValue = 0;
+    private double OletterValue = 0;
+    private double MletterValue = 0;
+    private double EletterValue = 0;
+    private double TletterValue = 0;
+    private double HletterValue = 0;
+    private double IletterValue = 0;
+    private double NletterValue = 0;
+    private double GletterValue = 0;
 
+    private int letterValueS;
+    private int letterValueO;
+    private int letterValueM;
+    private int letterValueE;
+    private int letterValueT;
+    private int letterValueH;
+    private int letterValueI;
+    private int letterValueN;
+    private int letterValueG;
+    public double[] orderOfLetter = new double[9];
+
+    private string serialNumber;
+    private string serialLetters;
 
     private static int moduleIdCounter = 1;
     private int moduleId;
     private bool _isSolved;
+    private bool itIsTime = false;
+
+    private bool ser1 = false;
+    private bool ser2 = false;
+    private bool ser3 = false;
+    private bool ser4 = false;
+    private bool ser5 = false;
+    private bool ser6 = false;
+    private bool ser7 = false;
+    private bool ser8 = false;
+    private bool ser9 = false;
 
 
     // Use this for initialization
     void Start () {
         moduleId = moduleIdCounter++;
-        bombModule.OnActivate += Activate;
         status = false;
+        ser1 = false;
+        ser2 = false;
+        ser3 = false;
+        ser4 = false;
+        ser5 = false;
+        ser6 = false;
+        ser7 = false;
+        ser8 = false;
+        ser9 = false;
         audio = GetComponent<KMAudio>();
-        letterValues = 1;
+        letterValues = 0;
+        bombModule.OnActivate += Activate;
+        obtainSerial();
+        sumOfDigits = Bomb.GetSerialNumberNumbers().Sum();
         ColorRender[0].material = colorful[0];
         ColorRender[1].material = colorful[0];
         ColorRender[2].material = colorful[0];
@@ -73,9 +122,10 @@ public class literallySomethingScript : MonoBehaviour {
         ColorRender[7].material = colorful[0];
         ColorRender[8].material = colorful[0];
     }
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update ()
+    {
+        #region Update
         if (status == true)
         {
             Status.gameObject.SetActive(true);
@@ -85,11 +135,187 @@ public class literallySomethingScript : MonoBehaviour {
             Status.gameObject.SetActive(false);
         }
 
-        if (letterValues == 10)
+        if (letterValues == 9)
         {
             SolveOrNot();
         }
+        #endregion
+    }
+    void obtainSerial()
+    {
+        serialNumber = Bomb.GetSerialNumber();
+        serialLetters = Regex.Replace(serialNumber, @"[\d-]", string.Empty);
+        serialLetters = serialLetters + serialLetters + serialLetters + serialLetters;
+        serialLetters = serialLetters.Remove(9);
+        serialProcess();
+    }
 
+    void serialProcess()
+    {
+        char[] separateSerialLetters = serialLetters.ToCharArray();
+        int[] serialNumeral = Array.ConvertAll(separateSerialLetters, c => (int)Char.GetNumericValue(c));
+        StringBuilder st = new StringBuilder();
+        for (int i = 0; i < separateSerialLetters.Length; i++)
+        {
+            st.Append(separateSerialLetters[i]);
+        }
+        serInt1 = st[0] - 64;
+        serInt2 = st[1] - 64;
+        serInt3 = st[2] - 64;
+        serInt4 = st[3] - 64;
+        serInt5 = st[4] - 64;
+        serInt6 = st[5] - 64;
+        serInt7 = st[6] - 64;
+        serInt8 = st[7] - 64;
+        serInt9 = st[8] - 64;
+
+        Adding();
+    }
+
+    void Adding()
+    {
+        serInt1 = serInt1 + 19;
+        serInt2 = serInt2 + 15;
+        serInt3 = serInt3 + 13;
+        serInt4 = serInt4 + 5;
+        serInt5 = serInt5 + 20;
+        serInt6 = serInt6 + 8;
+        serInt7 = serInt7 + 9;
+        serInt8 = serInt8 + 14;
+        serInt9 = serInt9 + 7;
+
+        
+
+        Rascal();
+    }
+
+    void Rascal()
+    {
+        if (serInt2 == serInt1)
+        {
+            serInt2 = serInt2 + 1;
+        }
+        if (serInt3 == serInt2 || serInt3 == serInt1)
+        {
+            serInt3 = serInt3 + 1;
+        }
+        if (serInt4 == serInt3 || serInt4 == serInt2 || serInt4 == serInt1)
+        {
+            serInt4 = serInt4 + 1;
+        }
+        if (serInt5 == serInt4 || serInt5 == serInt3 || serInt5 == serInt2 || serInt5 == serInt1)
+        {
+            serInt5 = serInt5 + 1;
+        }
+        if (serInt6 == serInt5 || serInt6 == serInt4 || serInt6 == serInt3 || serInt6 == serInt2 || serInt6 == serInt1)
+        {
+            serInt6 = serInt6 + 1;
+        }
+        if (serInt7 == serInt6 || serInt7 == serInt5 || serInt7 == serInt4 || serInt7 == serInt3 || serInt7 == serInt2 || serInt7 == serInt1)
+        {
+            serInt7 = serInt7 + 1;
+        }
+        if (serInt8 == serInt7 || serInt8 == serInt6 || serInt8 == serInt5 || serInt8 == serInt4 || serInt8 == serInt3 || serInt8 == serInt2 || serInt8 == serInt1)
+        {
+            serInt8 = serInt8 + 1;
+        }
+        if (serInt9 == serInt8 || serInt9 == serInt7 || serInt9 == serInt6 || serInt9 == serInt5 || serInt9 == serInt4 || serInt9 == serInt3 || serInt9 == serInt2 || serInt9 == serInt1)
+        {
+            serInt9 = serInt9 + 1;
+        }
+
+        if (serInt2 == serInt1)
+        {
+            serInt2 = serInt2 + 1;
+        }
+        if (serInt3 == serInt2 || serInt3 == serInt1)
+        {
+            serInt3 = serInt3 + 1;
+        }
+        if (serInt4 == serInt3 || serInt4 == serInt2 || serInt4 == serInt1)
+        {
+            serInt4 = serInt4 + 1;
+        }
+        if (serInt5 == serInt4 || serInt5 == serInt3 || serInt5 == serInt2 || serInt5 == serInt1)
+        {
+            serInt5 = serInt5 + 1;
+        }
+        if (serInt6 == serInt5 || serInt6 == serInt4 || serInt6 == serInt3 || serInt6 == serInt2 || serInt6 == serInt1)
+        {
+            serInt6 = serInt6 + 1;
+        }
+        if (serInt7 == serInt6 || serInt7 == serInt5 || serInt7 == serInt4 || serInt7 == serInt3 || serInt7 == serInt2 || serInt7 == serInt1)
+        {
+            serInt7 = serInt7 + 1;
+        }
+        if (serInt8 == serInt7 || serInt8 == serInt6 || serInt8 == serInt5 || serInt8 == serInt4 || serInt8 == serInt3 || serInt8 == serInt2 || serInt8 == serInt1)
+        {
+            serInt8 = serInt8 + 1;
+        }
+        if (serInt9 == serInt8 || serInt9 == serInt7 || serInt9 == serInt6 || serInt9 == serInt5 || serInt9 == serInt4 || serInt9 == serInt3 || serInt9 == serInt2 || serInt9 == serInt1)
+        {
+            serInt9 = serInt9 + 1;
+        }
+
+        if (serInt2 == serInt1)
+        {
+            serInt2 = serInt2 + 1;
+        }
+        if (serInt3 == serInt2 || serInt3 == serInt1)
+        {
+            serInt3 = serInt3 + 1;
+        }
+        if (serInt4 == serInt3 || serInt4 == serInt2 || serInt4 == serInt1)
+        {
+            serInt4 = serInt4 + 1;
+        }
+        if (serInt5 == serInt4 || serInt5 == serInt3 || serInt5 == serInt2 || serInt5 == serInt1)
+        {
+            serInt5 = serInt5 + 1;
+        }
+        if (serInt6 == serInt5 || serInt6 == serInt4 || serInt6 == serInt3 || serInt6 == serInt2 || serInt6 == serInt1)
+        {
+            serInt6 = serInt6 + 1;
+        }
+        if (serInt7 == serInt6 || serInt7 == serInt5 || serInt7 == serInt4 || serInt7 == serInt3 || serInt7 == serInt2 || serInt7 == serInt1)
+        {
+            serInt7 = serInt7 + 1;
+        }
+        if (serInt8 == serInt7 || serInt8 == serInt6 || serInt8 == serInt5 || serInt8 == serInt4 || serInt8 == serInt3 || serInt8 == serInt2 || serInt8 == serInt1)
+        {
+            serInt8 = serInt8 + 1;
+        }
+        if (serInt9 == serInt8 || serInt9 == serInt7 || serInt9 == serInt6 || serInt9 == serInt5 || serInt9 == serInt4 || serInt9 == serInt3 || serInt9 == serInt2 || serInt9 == serInt1)
+        {
+            serInt9 = serInt9 + 1;
+        }
+
+        numb[0] = serInt1;
+        numb[1] = serInt2;
+        numb[2] = serInt3;
+        numb[3] = serInt4;
+        numb[4] = serInt5;
+        numb[5] = serInt6;
+        numb[6] = serInt7;
+        numb[7] = serInt8;
+        numb[8] = serInt9;
+
+        //Thank you Goofy! I love you <3
+
+        var result = numb
+        .Select((ch, ix) => new { Character = ch, Index = ix })
+        .OrderBy(inf => inf.Character)
+        .Select((inf, ix) => new { inf.Index, Character = (char)(ix + '1') })
+        .OrderBy(inf => inf.Index)
+        .Select(inf => inf.Character)
+        .Join("");
+        for (int i = 0; i < numb.Length; i++)
+        {
+            numb[i] = char.GetNumericValue(result[i]);
+            numb[i] = numb[i] - 1;
+        }
+
+        Debug.LogFormat("[Literally Something #{0}] S = {1} , O = {2} , M = {3} , E = {4} , T = {5} , H = {6} , I = {7} , N = {8} , G = {9}.", moduleId, numb[0], numb[1], numb[2], numb[3], numb[4], numb[5], numb[6], numb[7], numb[8]);
     }
 
     void Activate()
@@ -118,9 +344,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] S.", moduleId);
-        if(SletterValue == 0)
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
+        if (SletterValue == 0)
         {
-            SletterValue = letterValues;
+            SletterValue = 1;
+            letterValueS = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[0].material = colorful[1];
@@ -131,9 +359,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] O.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (OletterValue == 0)
         {
-            OletterValue = letterValues;
+            OletterValue = 1;
+            letterValueO = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[1].material = colorful[1];
@@ -144,9 +374,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] M.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (MletterValue == 0)
         {
-            MletterValue = letterValues;
+            MletterValue = 1;
+            letterValueM = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[2].material = colorful[1];
@@ -157,9 +389,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] E.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (EletterValue == 0)
         {
-            EletterValue = letterValues;
+            EletterValue = 1;
+            letterValueE = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[3].material = colorful[1];
@@ -170,9 +404,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] T.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (TletterValue == 0)
         {
-            TletterValue = letterValues;
+            TletterValue = 1;
+            letterValueT = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[4].material = colorful[1];
@@ -183,9 +419,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] H.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (HletterValue == 0)
         {
-            HletterValue = letterValues;
+            HletterValue = 1;
+            letterValueH = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[5].material = colorful[1];
@@ -196,9 +434,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] I.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (IletterValue == 0)
         {
-            IletterValue = letterValues;
+            IletterValue = 1;
+            letterValueI = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[6].material = colorful[1];
@@ -209,9 +449,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] N.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] {1}.", moduleId, letterValues);
         if (NletterValue == 0)
         {
-            NletterValue = letterValues;
+            NletterValue = 1;
+            letterValueN = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[7].material = colorful[1];
@@ -222,9 +464,11 @@ public class literallySomethingScript : MonoBehaviour {
         audio.PlayGameSoundAtTransform(KMSoundOverride.SoundEffect.ButtonPress, transform);
         GetComponent<KMSelectable>().AddInteractionPunch();
         Debug.LogFormat("[Literally Something #{0}] G.", moduleId);
+        Debug.LogFormat("[Literally Something #{0}] PINGAS {1}.", moduleId, letterValues);
         if (GletterValue == 0)
         {
-            GletterValue = letterValues;
+            GletterValue = 1;
+            letterValueG = letterValues;
             letterValues = (letterValues + 1);
         }
         ColorRender[8].material = colorful[1];
@@ -232,7 +476,10 @@ public class literallySomethingScript : MonoBehaviour {
 
     void SolveOrNot()
     {
-        if (letterValues == 10 && SletterValue == 1 && OletterValue == 2 && MletterValue == 3 && EletterValue == 4 && TletterValue == 5 && HletterValue == 6 && IletterValue == 7 && NletterValue == 8 && GletterValue == 9)
+
+        Debug.LogFormat("[Literally Something #{0}] S = {1} , O = {2} , M = {3} , E = {4} , T = {5} , H = {6} , I = {7} , N = {8} , G = {9}.", moduleId, numb[0], numb[1], numb[2], numb[3], numb[4], numb[5], numb[6], numb[7], numb[8]);
+
+        if (letterValues == 9 && letterValueS == numb[0] && letterValueO == numb[1] && letterValueM == numb[2] && letterValueE == numb[3] && letterValueT == numb[4] && letterValueH == numb[5] && letterValueI == numb[6] && letterValueN == numb[7] && letterValueG == numb[8])
         {
             bombModule.HandlePass();
             _isSolved = true;
@@ -252,6 +499,8 @@ public class literallySomethingScript : MonoBehaviour {
         }
         else
         {
+            letterValues = 0;
+            Debug.LogFormat("[Literally Something #{0}] {1}", moduleId, letterValues);
             bombModule.HandleStrike();
             StartCoroutine(AnimateStrike());
         }
@@ -268,7 +517,15 @@ public class literallySomethingScript : MonoBehaviour {
         IletterValue = 0;
         NletterValue = 0;
         GletterValue = 0;
-        letterValues = 1;
+        letterValueS = 0;
+        letterValueO = 0;
+        letterValueM = 0;
+        letterValueE = 0;
+        letterValueT = 0;
+        letterValueH = 0;
+        letterValueI = 0;
+        letterValueN = 0;
+        letterValueG = 0;
         ColorRender[0].material = colorful[2];
         ColorRender[1].material = colorful[2];
         ColorRender[2].material = colorful[2];
